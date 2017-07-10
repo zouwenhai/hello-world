7fd99da93087e416b467c6e6e5483a45984f5d99
xx <305526954@qq.com>
2017-07-10 14:58:12 +0800

    Django基本配置完成，开始使用git管理

---
+++ woks/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WoksConfig(AppConfig):
+    name = 'woks'
---
+++ yax/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class YaxConfig(AppConfig):
+    name = 'yax'
