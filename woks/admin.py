c5558b5adba951c5afb83d9893c06b54686ad642
xx <305526954@qq.com>
2017-07-13 08:45:43 +0800

    woks项目，可以录入文章，并简单显示，以便查看markdown格式

--- woks/admin.py
+++ woks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+from .models import Series
+from .models import Chapter
+
+
+admin.site.register(Category)
+admin.site.register(Series)
+admin.site.register(Chapter)
--- woks/models.py
+++ woks/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    rank = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Series(models.Model):
+
+    category = models.ForeignKey(Category)
+
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    rank = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Chapter(models.Model):
+
+    series = models.ForeignKey(Series)
+
+    number = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    rank = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.number + ' ' + self.title
