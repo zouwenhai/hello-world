8458010a5b23b5c3b7493ebe95a0fe97df92a437
xx <305526954@qq.com>
2018-02-27 17:34:49 +0800

    暂时增加一个记录账号的model

--- ganx/models.py
+++ ganx/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+
+    platform = models.CharField(max_length=24)
+    username = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now_add=True)
